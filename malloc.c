ff0d9f8509b0dd99fde6ce94b75a50cc7c9aa510
Husnain56 <husnainhere.2019.5@gmail.com>
2026-08-03 04:55:56 +0500

    feat: add basic bump allocator with fixed-size heap buffer

---
+++ malloc.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define CAPACITY 640000
+
+char HEAP[CAPACITY];
+
+size_t current_size = 0;
+
+void* heap_alloc(size_t size){
+    
+    if(current_size >= CAPACITY){
+       
+        return NULL;    
+    }       
+    
+    char* ptr = &HEAP[current_size];
+    current_size += size;
+    return ptr;
+}
+
+int main(){
+
+    
+
+    char * pointer = heap_alloc(2);
+    char * ptr = heap_alloc(1);
+
+    printf("Memory address of first pointer: %p\n", (void*)pointer);
+    printf("Memory address of second pointer: %p", (void*)ptr);
+
+    pointer = NULL;
+    ptr = NULL;
+        
+    return 0;
+}
